a02a133620c6a3b50d624fcffaf5be67f6a4210a
liafernando <91317153+liafernando@users.noreply.github.com>
2025-04-09 11:26:05 +0200

    delete auth route

--- netflix-clone-backend/src/routes/auth.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { refreshToken } from "../controllers/auth.controller";
-
-const router = express.Router();
-
-router.post("/refresh-token", refreshToken);
-
-export const authRouter = router;
